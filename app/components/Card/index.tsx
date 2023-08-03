0eda4007862146682d189f17adca0c94356f2d22
Valentina <valentinab198@gmail.com>
2023-08-03 18:53:01 +0200

    Added animation at hover

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -22,56 +22,62 @@ const Card = ({
   regionText,
 }: ICard) => {
   return (
-    <section>
-      <div className="grid sm:grid-cols-4 sm:w-full w-[300px] grid-cols-1 my-0 mx-auto gap-[74px] ">
-        {React.Children.toArray(
-          data?.slice(indexOfFistCard, indexOfLastCard).map((i) => {
-            return (
-              <div
-                className="grid grid-cols-1 w-full  bg-white  dark:bg-darkBlue  h-[336px] rounded-md overflow-hidden"
-                style={{ boxShadow: "0px 0px 7px 2px rgba(0, 0, 0, 0.03)" }}
-              >
-                <div className="w-full relative h-[160px]">
-                  <Image
-                    className="object-cover w-full"
-                    unoptimized
-                    loader={imageLoader}
-                    src={i.flags.png}
-                    alt={i.name.common}
-                    objectFit="cover"
-                    layout="fill"
-                    priority
-                  />
-                </div>
+    <>
+      {data.length > 0 ? (
+        <div className="grid md:grid-cols-3 lg:grid-cols-4 sm:w-full w-[300px]  grid-cols-1 my-0 mx-auto gap-[74px] ">
+          {React.Children.toArray(
+            data?.slice(indexOfFistCard, indexOfLastCard).map((i) => {
+              return (
+                <div
+                  className="grid grid-cols-1 w-full  bg-white  dark:bg-darkBlue  h-[336px] rounded-md overflow-hidden"
+                  style={{ boxShadow: "0px 0px 7px 2px rgba(0, 0, 0, 0.03)" }}
+                >
+                  <div className="w-full relative h-[160px]">
+                    <Image
+                      className="object-cover w-full"
+                      unoptimized
+                      loader={imageLoader}
+                      src={i.flags.png}
+                      alt={i.name.common}
+                      objectFit="cover"
+                      layout="fill"
+                      priority
+                    />
+                  </div>
 
-                <div className="grid grid-cols-1 p-[24px]  pb-[46px]">
-                  <Link href={`/details/${i.cca2}`}>
-                    <h3 className="text-black dark:text-white  text-[18px] font-extrabold leading-[26px] gap-[16px]">
-                      {i.name.common}
-                    </h3>
-                  </Link>
+                  <div className="grid grid-cols-1 p-[24px]  pb-[46px]">
+                    <Link href={`/details/${i.cca2}`}>
+                      <h3 className=" hover:text-[#06c] hover:underline  dark:hover:text-[#2997ff] text-black dark:text-white  text-[18px] font-extrabold leading-[26px] gap-[16px]">
+                        {i.name.common}
+                      </h3>
+                    </Link>
 
-                  <div className="grid grid-cols-1 text-black dark:text-white text-[14px] font-[600] leading-[16px] gap-[8px]">
-                    <p>
-                      {populationText}:
-                      <span className="font-[300]">{i.population}</span>
-                    </p>
-                    <p>
-                      {regionText}:{" "}
-                      <span className="font-[300]">{i.region}</span>
-                    </p>
-                    <p>
-                      {capitalText}:{" "}
-                      <span className="font-[300]">{i.capital}</span>
-                    </p>
+                    <div className="grid grid-cols-1 text-black dark:text-white text-[14px] font-[600] leading-[16px] gap-[8px]">
+                      <p>
+                        {populationText}:{" "}
+                        <span className="font-[300]">{i.population}</span>
+                      </p>
+                      <p>
+                        {regionText}:{" "}
+                        <span className="font-[300]">{i.region}</span>
+                      </p>
+                      <p>
+                        {capitalText}:{" "}
+                        <span className="font-[300]">{i.capital}</span>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })
-        )}
-      </div>
-    </section>
+              );
+            })
+          )}
+        </div>
+      ) : (
+        <div className="w-full text-center h-[100vh]">
+          <h3 className="font-[600] text-[24px]">No result</h3>
+        </div>
+      )}
+    </>
   );
 };
 
--- app/components/SearchInput/index.tsx
+++ app/components/SearchInput/index.tsx
@@ -10,14 +10,18 @@ interface ISearchInput {
 const SearchInput = ({ placeHolderText, onChange }: ISearchInput) => {
   return (
     <form
-      className="flex justify-start items-center gap-6 border-none w-full relative min-w-[343px]  sm:w-[480px] h-[56px] bg-white dark:bg-darkBlue rounded-[5px] py-[19px] px-[32px]"
+      className="
+    
+      hover:animate-pulse
+      flex justify-start items-center gap-6 border-none w-full relative min-w-[343px]  sm:w-[480px] h-[56px] bg-white dark:bg-darkBlue rounded-[5px] py-[19px] px-[32px]"
       style={{ boxShadow: "0px 2px 9px 0px rgba(0, 0, 0, 0.05)" }}
     >
       <SearchIconSvg className={"fill-darkGray dark:fill-white"} />
 
       <input
         onChange={(e) => onChange(e)}
-        className="focus:outline-none sm:placeholder:text-[14px] placeholder:text-xs placeholder:leading-[20px] placeholder:font-[400] placeholder:text-darkGray dark:placeholder:text-white bg-transparent"
+        className="
+       focus:outline-none sm:placeholder:text-[14px] placeholder:text-xs placeholder:leading-[20px] placeholder:font-[400] placeholder:text-darkGray dark:placeholder:text-white bg-transparent"
         type="text"
         placeholder={placeHolderText}
       />
