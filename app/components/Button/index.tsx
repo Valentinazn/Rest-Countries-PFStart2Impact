e64d4e67fd29297fafc30545aa45fff281f928fe
Valentina <valentinab198@gmail.com>
2023-08-05 11:50:55 +0200

    Improved UI

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -8,7 +8,11 @@ interface IButton {
 const Button = ({ disabled, onClick, text }: IButton) => {
   return (
     <button
-      className="transition-all dark:hover:bg-[#3a4b5c] duration-0o3 ease-in-out rounded-[5px] bg-white dark:bg-darkBlue leading-[20px] p-[10px] disabled:cursor-not-allowed cursor-pointer"
+      className={`${
+        disabled
+          ? "hover:opacity-20"
+          : "hover:bg-darkGray hover:text-white dark:hover:bg-[#3a4b5c]"
+      } transition-all  duration-0o3 ease-in-out rounded-[5px] bg-white dark:bg-darkBlue leading-[20px] p-[10px] disabled:cursor-not-allowed cursor-pointer`}
       disabled={disabled}
       style={{ boxShadow: "0px 2px 9px 0px rgba(0, 0, 0, 0.05)" }}
       onClick={(e) => onClick(e)}
